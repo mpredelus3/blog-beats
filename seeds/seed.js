691cda0d574fa1f54ab7ce2fb53c8aa886d316d7
mpredelus3 <mpredelus3@gmail.com>
2024-06-10 21:28:08 -0700

    cleaned homepage, removed default postsat homepage

--- seeds/seed.js
+++ seeds/seed.js
@@ -3,7 +3,7 @@ const { User, Blog } = require('../models');
 
 // imports the user and project data
 const userData = require('./userData.json');
-const blogData = require('./blogData.json');
+// const blogData = require('./blogData.json');
 
 // function to seed the database with user and project data
 const seedDatabase = async () => {
@@ -14,12 +14,12 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const blog of blogData) {
-    await Blog.create({
-      ...blog,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const blog of blogData) {
+  //   await Blog.create({
+  //     ...blog,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
